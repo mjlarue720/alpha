159ca1517cbffa9a323aec4c479a684a9744646f
Brian Ragazzi <brian@zimage.com>
2012-09-08 17:27:34 -0700

    Fixed bugs porting from original app.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,14 +3,16 @@ import java.util.logging.Logger
 class BootStrap {
 
 
-  private static Logger log = (Logger)Logger.getInstance(monitor.AppControl)
+  //private static Logger log = (Logger)Logger.getInstance(alpha.AppControl)
 
   def init = { servletContext ->
 
     if(alpha.AppControl.firstBoot()){
-      log.info("Alpha bootrap: new start ${new Date()}")
+      //log.info("Alpha bootrap: new start ${new Date()}")
+      println("Alpha bootrap: new start ${new Date()}")
     }
-    log.info("Alpha bootrap: startup complete ${new Date()}")
+    //log.info("Alpha bootrap: startup complete ${new Date()}")
+    println("Alpha bootrap: startup complete ${new Date()}")
   }
 
 
--- sql/init.sql
+++ sql/init.sql
@@ -8,15 +8,15 @@ grant all on alpha_test.* to 'util'@'localhost' identified by '';
 drop database if exists alpha_prod;
 create database alpha_prod;
 grant all on alpha_prod.* to 'gvamos'@'localhost' identified by '';
-grant all on monitor_prod.* to 'tomcat'@'localhost' identified by '';
-grant all on monitor_prod.* to 'test'@'localhost' identified by '';
-grant all on monitor_prod.* to 'util'@'localhost' identified by '';
+grant all on alpha_prod.* to 'tomcat'@'localhost' identified by '';
+grant all on alpha_prod.* to 'test'@'localhost' identified by '';
+grant all on alpha_prod.* to 'util'@'localhost' identified by '';
 
-drop database if exists monitor_dev;
-create database monitor_dev;
-grant all on monitor_dev.* to 'gvamos'@'localhost' identified by '';
-grant all on monitor_dev.* to 'tomcat'@'localhost' identified by '';
-grant all on monitor_dev.* to 'test'@'localhost' identified by '';
-grant all on monitor_dev.* to 'util'@'localhost' identified by '';
+drop database if exists alpha_dev;
+create database alpha_dev;
+grant all on alpha_dev.* to 'gvamos'@'localhost' identified by '';
+grant all on alpha_dev.* to 'tomcat'@'localhost' identified by '';
+grant all on alpha_dev.* to 'test'@'localhost' identified by '';
+grant all on alpha_dev.* to 'util'@'localhost' identified by '';
 flush privileges;
 
